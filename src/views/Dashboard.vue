fd1c2418ff0def54fb5f67f62993e0b8012e86a6
ecunhado <eddcmac1999@gmail.com>
2022-03-05 19:24:02 +0000

    added main pages of company-side front-end

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dashboard">
+    Main Dashboard
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'main-dashboard',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+</style>
