f16bf7389b9fd791594ccb819b41f34a90938670
ecunhado <eddcmac1999@gmail.com>
2022-03-05 19:35:09 +0000

    deleted standard pages

---
+++ src/views/activities/ActivitiesDashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="activities-dashboard">
+    Activities Dashboard
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'main-dashboard',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+</style>
